82e83fa8a156f3b552198e967cdacfcce192b38b
hu.xiongfei <hu.xiongfei@topka.cn>
2014-12-23 19:46:40 +0800

    gulp init

---
+++ gulp/gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp'),
+  connect = require('gulp-connect');
+ 
+gulp.task('server', function() {
+  connect.server({
+    livereload: true,
+    middleware: function(connect, opt) {
+      return [ (function() {
+        var url = require('url');
+        var proxy = require('proxy-middleware');
+        var options = url.parse('http://topka.cn/');
+        options.route = '/api';
+        return proxy(options);
+      })()]
+    }
+  });
+});
+
+
+gulp.task('html', function () {
+  gulp.src('./*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./*.html', ['html']);
+})
+ 
+gulp.task('default', ['server','watch']);
